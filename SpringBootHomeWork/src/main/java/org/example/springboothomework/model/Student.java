94ec56c13653aae336d13c35e0dbcd5d8668dc33
Yusif Xankisiyev <yusif2006xankisiyev@gmail.com>
2026-03-06 15:15:52 +0400

    feature: add validation student class.

--- SpringBootHomeWork/src/main/java/org/example/springboothomework/model/Student.java
+++ SpringBootHomeWork/src/main/java/org/example/springboothomework/model/Student.java
@@ -1,12 +1,26 @@
 package org.example.springboothomework.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Student {
     private static int counter = 1;
 
     private String studentId;
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Address cannot be empty")
+    @Size(min = 5, max = 100, message = "Address must be between 5 and 100 characters")
     private String address;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
+
     private Course course;
 
     public Student(String name, String address, String email) {
