ff90aea99fea1d10badcc2b5f2b652c46f630842
Ilkin Ismayilov <ismayilovilkin2006@gmail.com>
2026-03-06 14:48:45 +0400

    First Commit

---
+++ SpringBootHomeWork/src/main/java/org/example/springboothomework/SpringBootHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboothomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHomeWorkApplication.class, args);
+    }
+
+}
---
+++ SpringBootHomeWork/src/test/java/org/example/springboothomework/SpringBootHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboothomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
