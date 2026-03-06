de9e0d8bfe5aba00fbc8b895bc25ae29bfe633c8
Yusif Xankisiyev <yusif2006xankisiyev@gmail.com>
2026-03-06 14:55:27 +0400

    featre: Student class added.

---
+++ SpringBootHomeWork/src/main/java/org/example/springboothomework/model/Student.java
@@ -0,0 +1,58 @@
+package org.example.springboothomework.model;
+
+public class Student {
+    private static int counter = 1;
+
+    private String studentId;
+    private String name;
+    private String address;
+    private String email;
+    private Course course;
+
+    public Student(String name, String address, String email) {
+        this.studentId = String.valueOf(counter++);
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
