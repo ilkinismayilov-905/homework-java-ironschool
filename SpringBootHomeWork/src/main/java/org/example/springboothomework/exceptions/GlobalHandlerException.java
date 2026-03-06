9a87bb1a8da7c88b1e712e0a723410156772e542
Ilkin Ismayilov <ismayilovilkin2006@gmail.com>
2026-03-06 15:47:38 +0400

    feature: Exception added,TeacherService added

--- SpringBootHomeWork/src/main/java/org/example/springboothomework/exceptions/GlobalHandlerException.java
+++ SpringBootHomeWork/src/main/java/org/example/springboothomework/exceptions/GlobalHandlerException.java
@@ -1,4 +1,20 @@
 package org.example.springboothomework.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalHandlerException {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String,String> handleProductNotFound(NotFoundException ex){
+        Map<String,String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
 }
--- SpringBootHomeWork/src/main/java/org/example/springboothomework/service/TeacherService.java
+++ SpringBootHomeWork/src/main/java/org/example/springboothomework/service/TeacherService.java
@@ -1,4 +1,53 @@
 package org.example.springboothomework.service;
 
+import org.example.springboothomework.exceptions.NotFoundException;
+import org.example.springboothomework.model.Teacher;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class TeacherService {
+
+    private final Map<String, Teacher> teachers = new HashMap<>();
+
+    public Teacher createTeacher(Teacher teacher) {
+        teachers.put(teacher.getTeacherId(), teacher);
+        return teacher;
+    }
+
+    public Collection<Teacher> getAllTeachers() {
+        return teachers.values();
+    }
+
+    public Teacher getTeacherById(String teacherId) {
+        Teacher teacher = teachers.get(teacherId);
+        if (teacher == null) {
+            throw new NotFoundException("Teacher not found");
+        }
+        return teacher;
+    }
+
+    public Teacher updateTeacher(String teacherId, Teacher updatedTeacher) {
+        Teacher teacher = getTeacherById(teacherId);
+        teacher.setName(updatedTeacher.getName());
+        teacher.setSalary(updatedTeacher.getSalary());
+        return teacher;
+    }
+
+    public void deleteTeacher(String teacherId) {
+        if (!teachers.containsKey(teacherId)) {
+            throw new NotFoundException("Teacher not found");
+        }
+        teachers.remove(teacherId);
+    }
+
+    public double getTotalSalary() {
+        return teachers.values()
+                .stream()
+                .mapToDouble(Teacher::getSalary)
+                .sum();
+    }
 }
